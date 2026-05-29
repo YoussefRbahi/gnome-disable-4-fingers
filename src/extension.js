fb508bc33252138395ccfafc0650faf1d0d14fbd
Youssef Rbahi <72280234+YoussefRbahi@users.noreply.github.com>
2026-05-29 15:56:53 +0100

    Refactor: Use connectObject/disconnectObject per GNOME review and rename main Class

--- src/extension.js
+++ src/extension.js
@@ -1,22 +1,19 @@
 import Clutter from 'gi://Clutter';
 import { Extension } from 'resource:///org/gnome/shell/extensions/extension.js';
 
-export default class DisableFourFingerExtension extends Extension {
+export default class DisableFourFingerGesturesExtension extends Extension {
     enable() {
-        this._eventId = global.stage.connect('captured-event', (actor, event) => {
+        this._eventId = global.stage.connectObject('captured-event', (actor, event) => {
             if (event.type() === Clutter.EventType.TOUCHPAD_SWIPE) {
                 if (event.get_touchpad_gesture_finger_count() === 4) {
                     return Clutter.EVENT_STOP; 
                 }
             }
             return Clutter.EVENT_PROPAGATE; 
-        });
+        }, this);
     }
 
     disable() {
-        if (this._eventId) {
-            global.stage.disconnect(this._eventId);
-            this._eventId = null;
-        }
+        global.stage.disconnectObject(this);
     }
 }
